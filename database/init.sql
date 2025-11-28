0c31eb96cf7180f70e139d5c4d3368a2ab7a2a8f
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2025-11-28 21:22:18 +0700

    upload

--- database/init.sql
+++ database/init.sql
@@ -194,8 +194,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `cccd`, `dob`,
 (15, 'Hồ Bảo Nguyên', 'nguyenhb.22it@vku.udn.vn', 'Baonguyen-132', 5, '048204007137', '2004-02-13', 'Male', '', 'Tổ 33, Hòa Quý, Ngũ Hành Sơn, Đà Nẵng', 417910, 'some_token', '2025-02-21 14:44:42', '2025-02-21 14:44:42'),
 (17, 'Hồ Thăng Bản', 'baonguyen182pht@gmail.com', '1234', 4, '048066007160', '1966-01-01', 'Male', '', 'Tổ 33, Hòa Quý, Ngũ Hành Sơn, Hồ Chí Minh', 32299, 'some_token', '2025-05-02 03:31:21', '2025-05-02 03:31:21'),
 (20, 'Trần Lê Hoàng Long', 'longtran201014@gmail.com', 'HoangLong-123', 4, '048210003173', '2010-10-01', 'Male', '', 'Thôn Phú Sơn Nam, Hòa Khương, Hòa Vang, Đà Nẵng', 42925, 'some_token', '2025-09-15 04:23:54', '2025-09-15 04:23:54'),
-(21, 'Nguyễn Khánh My', 'khongiantopo@gmail.com', 'KhanhMy-123', 4, '048195008281', '1995-01-01', 'Female', '', 'Tổ 1 thôn yến nê 1, Hòa Tiến, Hòa Vang, Đà Nẵng', 29210, 'some_token', '2025-10-03 04:16:29', '2025-10-03 04:16:29');
-
+(21, 'Nguyễn Khánh My', 'khongiantopo@gmail.com', 'KhanhMy-123', 4, '048195008281', '1995-01-01', 'Female', '', 'Tổ 1 thôn yến nê 1, Hòa Tiến, Hòa Vang, Đà Nẵng', 29210, 'some_token', '2025-10-03 04:16:29', '2025-10-03 04:16:29'),
+(22, 'Nguyễn Thuý Mai', 'duoinuicholantu6x9@gmail.com', 'ThuyMai-123', 5, '048195008282', '1995-01-01', 'Female', '', 'Tổ 1 thôn yến nê 1, Hòa Tiến, Hòa Vang, Đà Nẵng', 29210, 'some_token', '2025-10-03 04:16:29', '2025-10-03 04:16:29');
 --
 -- Chỉ mục cho các bảng đã đổ
 --
