d47154377b4e12654059dc16521e0eb62946afef
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2026-05-28 16:32:00 +0700

    upload

--- database/init.sql
+++ database/init.sql
@@ -191,11 +191,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `cccd`, `dob`, `gender`, `pob`, `address`, `point`, `token`, `created_at`, `updated_at`) VALUES
-(15, 'Hồ Bảo Nguyên', 'nguyenhb.22it@vku.udn.vn', 'NguyenBao-2004', 5, '048204007137', '2004-02-13', 'Male', '', 'Tổ 33, Hòa Quý, Ngũ Hành Sơn, Đà Nẵng', 417910, 'some_token', '2025-02-21 14:44:42', '2025-02-21 14:44:42'),
-(17, 'Hồ Thăng Bản', 'baonguyen182pht@gmail.com', '1234', 4, '048066007160', '1966-01-01', 'Male', '', 'Tổ 33, Hòa Quý, Ngũ Hành Sơn, Hồ Chí Minh', 32299, 'some_token', '2025-05-02 03:31:21', '2025-05-02 03:31:21'),
-(20, 'Trần Lê Hoàng Long', 'longtran201014@gmail.com', 'HoangLong-123', 4, '048210003173', '2010-10-01', 'Male', '', 'Thôn Phú Sơn Nam, Hòa Khương, Hòa Vang, Đà Nẵng', 42925, 'some_token', '2025-09-15 04:23:54', '2025-09-15 04:23:54'),
-(21, 'Nguyễn Khánh My', 'khongiantopo@gmail.com', 'KhanhMy-123', 4, '048195008281', '1995-01-01', 'Female', '', 'Tổ 1 thôn yến nê 1, Hòa Tiến, Hòa Vang, Đà Nẵng', 29210, 'some_token', '2025-10-03 04:16:29', '2025-10-03 04:16:29'),
-(22, 'Nguyễn Thuý Mai', 'duoinuicholantu6x9@gmail.com', 'ThuyMai-123', 5, '048195008282', '1995-01-01', 'Female', '', 'Tổ 1 thôn yến nê 1, Hòa Tiến, Hòa Vang, Đà Nẵng', 29210, 'some_token', '2025-10-03 04:16:29', '2025-10-03 04:16:29');
+(15, 'Nguyễn Văn A', 's@gmail.com', '$2y$10$e0NRu7n9s1mXo5l3Zt8OeG8j1v6z5b9c4d2f1g0h3j4k5l6m7n8', 1, '123456789012', '2000-01-01', 'Nam', 'Hà Nội', '123 Đường ABC, Phường XYZ, Quận 1, TP.HCM', 100, NULL, '2025-10-01 12:00:00', '2025-10-01 12:00:00'),
+(16, 'Trần Thị B', 'b@gmail.com', '$2y$10$e0NRu7n9s1mXo5l3Zt8OeG8j1v6z5b9c4d2f1g0h3j4k5l6m7n8', 1, '123456789013', '2000-01-01', 'Nữ', 'Hà Nội', '456 Đường DEF, Phường UVW, Quận 2, TP.HCM', 100, NULL, '2025-10-01 12:00:00', '2025-10-01 12:00:00');
 --
 -- Chỉ mục cho các bảng đã đổ
 --
